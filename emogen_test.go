7bf606dee969c7adc0812729e639855a5f236a3b
Joren Van Onder <joren@jvo.sh>
2021-09-28 17:48:22 -0700

    Update tests

--- emogen_test.go
+++ emogen_test.go
@@ -15,77 +15,17 @@ package main
 
 // import "fmt"
 import (
-	"math"
+	//	"math"
 	"testing"
 )
 
-func testIncrementOverNumbers(t *testing.T, increment uint, currentNumber uint, numbers []bool) {
-	for i := 0; i < len(numbers); i++ {
-		currentNumber = getNextEmojiNumber(uint(len(numbers)), increment, currentNumber)
-
-		if numbers[currentNumber] {
-			t.Errorf("%d was already in the sequence", currentNumber)
-			break
+func TestRandomRange(t *testing.T) {
+	length := 2
+	for i := 0; i < 9_999; i++ {
+		x := randomIndex(uint(length))
+		if x < 0 || x >= length {
+			t.Errorf("%d is out of range", x)
+			break;
 		}
-
-		numbers[currentNumber] = true
-	}
-}
-
-func TestGetNextEmojiEven(t *testing.T) {
-	numbers := make([]bool, 128)
-	var currentNumber uint
-	var increment uint = 41
-
-	testIncrementOverNumbers(t, increment, currentNumber, numbers)
-}
-
-func TestGetNextEmojiOdd(t *testing.T) {
-	numbers := make([]bool, 129)
-	var currentNumber uint
-	var increment uint = 71
-
-	testIncrementOverNumbers(t, increment, currentNumber, numbers)
-}
-
-func TestGetEmojis1(t *testing.T) {
-	// For a 9 bit number we would have 3 groups of 3 bits each. The
-	// max number in each group is 2^3.
-	var number uint = 5<<6 | 6<<3 | 7
-	length := uint(math.Pow(float64(2), float64(3)))
-
-	n1, n2, n3 := getEmojiNumbers(number, length)
-
-	if n1 != 7 {
-		t.Errorf("n1 is %d, expected 7", n1)
-	}
-
-	if n2 != 6 {
-		t.Errorf("n2 is %d, expected 6", n2)
-	}
-
-	if n3 != 5 {
-		t.Errorf("n3 is %d, expected 5", n3)
-	}
-}
-
-func TestGetEmojis2(t *testing.T) {
-	// For a 9 bit number we would have 3 groups of 3 bits each. The
-	// max number in each group is 2^3.
-	var number uint = 7<<6 | 7<<3 | 7
-	length := uint(math.Pow(float64(2), float64(3)))
-
-	n1, n2, n3 := getEmojiNumbers(number, length)
-
-	if n1 != 7 {
-		t.Errorf("n1 is %d, expected 7", n1)
-	}
-
-	if n2 != 7 {
-		t.Errorf("n2 is %d, expected 7", n2)
-	}
-
-	if n3 != 7 {
-		t.Errorf("n3 is %d, expected 7", n3)
 	}
 }
